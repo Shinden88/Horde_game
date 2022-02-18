5e48508668fef7110d1f55a460fe57ba2a3300c8
Matija <matija_bozic@yahoo.com>
2022-02-18 23:23:14 +0000

    created enemy class

--- src/entities/Wizard.js
+++ src/entities/Wizard.js
@@ -1,34 +1,9 @@
-import Phaser from "phaser";
-import collidable from "../mixins/collidable";
+import Enemy from './Enemy';
 
-class Wizard extends Phaser.Physics.Arcade.Sprite {
+class Wizard extends Enemy {
   constructor(scene, x, y) {
     super(scene, x, y, "wizard");
 
-    scene.add.existing(this);
-    scene.physics.add.exisitng(this);
-
-    Object.assign(this, collidable);
-
-    this.init();
-  }
-
-  init() {
-    this.gravity = 500;
-    this.speed = 150;
-
-    this.body.setGravityY(this.gravity);
-
-    //adjust later once map done
-    this.setSize(20, 45);
-    this.setOffset(7, 20);
-
-
-    this.setCollideWorldBounds(true);
-    //adjust later once map done
-    this.setImmovable(true);
-
-    this.setOrigin(0.5, 1);
   }
 }
 
