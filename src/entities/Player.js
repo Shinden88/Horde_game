b31b78b9b27553b6fb2bf6bb140a3bcbedd6e48e
RajMazing <caseyraj92@gmail.com>
2022-02-18 15:43:15 -0600

    added player.js

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -1,1 +1,15 @@
-import Phaser from 'phaser;'
+import Phaser from 'phaser';
+
+
+
+
+class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y) {
+      super(scene, x, y, 'player');
+
+      scene.physics.add.existing(this);
+    }
+
+}
+
+export default Player;
