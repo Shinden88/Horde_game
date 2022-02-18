aa3e7cf27fa9d1a567d259aa562f436e5193bda2
Matija <matija_bozic@yahoo.com>
2022-02-18 23:15:44 +0000

    enemy size adjusted

--- src/entities/Wizard.js
+++ src/entities/Wizard.js
@@ -18,7 +18,16 @@ class Wizard extends Phaser.Physics.Arcade.Sprite {
     this.speed = 150;
 
     this.body.setGravityY(this.gravity);
+
+    //adjust later once map done
+    this.setSize(20, 45);
+    this.setOffset(7, 20);
+
+
     this.setCollideWorldBounds(true);
+    //adjust later once map done
+    this.setImmovable(true);
+
     this.setOrigin(0.5, 1);
   }
 }
