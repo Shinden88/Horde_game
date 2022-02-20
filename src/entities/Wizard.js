d74aad8012a1e2f112d9cc148b7d53894b5bbd0e
Caseyann <goore_44@hotmail.ca>
2022-02-19 22:19:14 -0500

    update enemies

--- src/entities/Wizard.js
+++ src/entities/Wizard.js
@@ -1,12 +1,15 @@
 import Enemy from './Enemy';
+import initializeAnimations from './Animations/WizardAnimation';
 
 class Wizard extends Enemy {
   constructor(scene, x, y) {
     super(scene, x, y, "wizard");
+    initializeAnimations(scene.anims);
+  }
 
-
-
-
+  update(time, delta) {
+    super.update(time,delta);
+    this.play('wizard-idle', true);
   }
 }
 
