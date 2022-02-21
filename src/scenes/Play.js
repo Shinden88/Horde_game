d76e6452a390af1e2b1c6157984e69e3f1f7b3f2
Caseyann <goore_44@hotmail.ca>
2022-02-21 00:24:46 -0500

    fix endzone

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -145,7 +145,7 @@ class Play extends Phaser.Scene {
     const endOfLevel = this.physics.add
       .sprite(end.x, end.y, "end")
       .setAlpha(0)
-      .setSize(5, this.config.height)
+      .setSize(5, 30)
       .setOrigin(0.5, 1);
 
     const endOverlap = this.physics.add.overlap(player, endOfLevel, () => {
