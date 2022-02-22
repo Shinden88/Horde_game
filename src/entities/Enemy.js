2b9ef63acc784879ba4f06c231ea8661c6603b58
Caseyann <goore_44@hotmail.ca>
2022-02-21 23:00:31 -0500

    enemy patrol updated

--- src/entities/Enemy.js
+++ src/entities/Enemy.js
@@ -1,86 +1,3 @@
-// import Phaser from "phaser";
-// import collidable from "../mixins/collidable";
-
-// class Enemy extends Phaser.Physics.Arcade.Sprite {
-//   constructor(scene, x, y, key) {
-//     super(scene, x, y, key);
-
-//     scene.add.existing(this);
-//     scene.physics.add.existing(this);
-
-//     Object.assign(this, collidable);
-
-//     this.init();
-//     this.initEvents();
-//   }
-
-//   init() {
-//     this.gravity = 500;
-//     this.speed = 150;
-//     this.platformCollidersLayer = null;
-//     this.rayGraphics = this.scene.add.graphics({lineStyle: {width: 2, color: 0xaa0aa}})
-
-//     this.body.setGravityY(this.gravity);
-
-//     //adjust later once map done
-//     this.setSize(60, 110);
-//     this.setDisplaySize(110, 110);
-//     this.setOffset(100, 55);
-
-
-//     this.setCollideWorldBounds(true);
-//     //adjust later once map done
-//     this.setImmovable(true);
-
-//     this.setOrigin(0.5, 1);
-//   }
-
-//   initEvents() {
-//     this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this)
-//   }
-
-//   update(time, delta) {
-//     this.setVelocityX(30);
-//     const { ray, hasHit } = this.raycast(this.body, this.platformCollidersLayer);
-//     // const {ray, hasHit} = this.raycast(this.body, this.platformCollidersLayer, {raylength: 50, precision: 1, steepness: 1.1});
-
-//     if (hasHit) {
-//       console.log('Hitting the platform');
-//     }
-
-//     this.rayGraphics.clear();
-//     this.rayGraphics.strokeLineShape(ray);
-
-//   }
-
-
-//   setPlatformColliders(platformCollidersLayer) {
-//     this.platformCollidersLayer = platformCollidersLayer;
-//   }
-
-//   raycast(body, setLayer, raylength = 30) {
-//     const { x, y, width, halfHeight } = body;
-//     const line = new Phaser.Geom.Line();
-//     let hasHit = false;
-
-//     line.x1 = x + width;
-//     line.y1 = y + halfHeight;
-//     line.x2 = line.x1 + raylength;
-//     line.y2 = line.y1 + raylength;
-
-
-//     const hits = setLayer.getTilesWithinShape(line);
-
-//     if (hits.length > 0) {
-//       hasHit = hits.some(hit => hit.index !== -1);
-//     }
-
-//     return { ray: line, hasHit };
-//   }
-// }
-
-// export default Enemy;
-
 import Phaser from 'phaser';
 
 import collidable from '../mixins/collidable';
@@ -101,7 +18,11 @@ class Enemy extends Phaser.Physics.Arcade.Sprite {
 
   init() {
     this.gravity = 500;
-    this.speed = 50;
+    this.speed = 70;
+    this.timeFromLastTurn = 0;
+    this.maxPatrolDistance = 200;
+    this.currentPatrolDistance = 0;
+
     this.platformCollidersLayer = null;
     this.rayGraphics = this.scene.add.graphics({lineStyle: {width: 2, color: 0xaa00aa}});
 
@@ -118,18 +39,27 @@ class Enemy extends Phaser.Physics.Arcade.Sprite {
     this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this)
   }
 
-  update(time, delta) {
-    // this.setVelocityX(30);
-    // const { ray, hasHit } = this.raycast(this.body, this.platformCollidersLayer);
-    const { ray, hasHit } = this.raycast(this.body, this.platformCollidersLayer, 30, 1);
+  update(time) {
+    this.patrol(time);
+  }
 
-    // if (hasHit) {
-    //   console.log('Hitting the platform!');
-    // }
+  patrol(time) {
 
-    if (hasHit) {
+    if (!this.body || !this.body.onFloor()) { return; }
+
+  
+
+    this.currentPatrolDistance += Math.abs(this.body.deltaX());
+
+    const { ray, hasHit } = this.raycast(this.body, this.platformCollidersLayer, {
+      precision: 1, steepnes: 0.4});
+
+    if ((hasHit || this.currentPatrolDistance >= this.maxPatrolDistance) &&
+    this.timeFromLastTurn + 100 < time) {
       this.setFlipX(!this.flipX);
       this.setVelocityX(this.speed = -this.speed);
+      this.timeFromLastTurn = time;
+      this.currentPatrolDistance = 0;
     }
 
     this.rayGraphics.clear();
@@ -139,25 +69,6 @@ class Enemy extends Phaser.Physics.Arcade.Sprite {
   setPlatformColliders(platformCollidersLayer) {
     this.platformCollidersLayer = platformCollidersLayer;
   }
-
-  // raycast(body, layer, raylength = 30) {
-  //   const { x, y, width, halfHeight } = body;
-  //   const line = new Phaser.Geom.Line();
-  //   let hasHit = false;
-
-  //   line.x1 = x + width;
-  //   line.y1 = y + halfHeight;
-  //   line.x2 = line.x1 + raylength;
-  //   line.y2 = line.y1 + raylength;
-
-  //   const hits = layer.getTilesWithinShape(line);
-
-  //   if (hits.length > 0) {
-  //     hasHit = hits.some(hit => hit.index !== -1);
-  //   }
-
-  //   return { ray: line, hasHit };
-  // }
 }
 
 
