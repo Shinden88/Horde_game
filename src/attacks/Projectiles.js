eb3b290b88b588dff6618f2f2b35055429053f51
RajMazing <caseyraj92@gmail.com>
2022-02-23 02:21:32 -0600

    centering the fireball

--- src/attacks/Projectiles.js
+++ src/attacks/Projectiles.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import Projectile from './Projectile';
+import { getTimestamp } from '../utils/functions';
 
 class Projectiles extends Phaser.Physics.Arcade.Group {
 
@@ -13,29 +14,33 @@ class Projectiles extends Phaser.Physics.Arcade.Group {
       key: 'iceball',
       classType: Projectile
     })
+
+    this.FromLastProjectile = null;
   }
 
   fireProjectile(initiator) {
     const projectile = this.getFirstDead(false);
 
     if (!projectile) { return; }
-    
-    
-    //const center = initiator.getCenter();
-    
 
+    if (this.timeFromLastProjectile &&
+      this.timeFromLastProjectile + projectile.cooldown > getTimestamp()) { return; }    
+    const center = initiator.getCenter();
+    
+      let centerX;
     if (initiator.lastDirection === Phaser.Physics.Arcade.FACING_RIGHT) {
       projectile.speed = Math.abs(projectile.speed);
       projectile.setFlipX(false);
-
+        centerX = center.x + 10;
     } else {
       projectile.speed = -Math.abs(projectile.speed);
       projectile.setFlipX(true);
+      centerX = center.x - 10;
     }
 
-    projectile.fire(initiator.x, initiator.y);
     
-    //projectile.fire(center.x, center.y);
+    projectile.fire(centerX, center.y);
+    this.timeFromLastProjectile = getTimestamp();
   }
 
 }
