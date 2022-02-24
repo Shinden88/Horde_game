a9fbabb4aeabeaf54b01822762edb05651cc87ea
Caseyann <goore_44@hotmail.ca>
2022-02-23 22:11:01 -0500

    back to before major bug

---
+++ server.js
@@ -0,0 +1,17 @@
+// const path = require('path');
+// const express = require('express');
+ 
+// const app = express();
+// const DIST_DIR = path.join(__dirname, '/build');
+// const HTML_FILE = path.join(DIST_DIR, 'index.html');
+ 
+// app.use(express.static(DIST_DIR));
+// app.get('*', (req, res) => {
+//   res.sendFile(HTML_FILE)
+// });
+ 
+// const PORT = process.env.PORT || 3001;
+ 
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}!`)
+// });
